f537eaed72a13a46408747cc9b9826094cb9ae8d
d_barnett <domoniquebarnett7@gmail.com>
2017-02-14 05:07:13 +0000

    flask exercise

--- app/views.py
+++ app/views.py
@@ -6,12 +6,13 @@ This file creates your application.
 """
 
 from app import app
-from flask import render_template, request, redirect, url_for
-
+from flask import render_template, request, redirect, url_for, flash
+import smtplib , os
 
 ###
 # Routing for your application.
 ###
+app.secret_key = os.urandom(24)
 
 @app.route('/')
 def home():
@@ -24,7 +25,41 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/contact', methods=['POST', 'GET'])
+def contact():
+    error=None
+    if request.method == 'POST':
+       from_name=request.form['name']
+       from_email=request.form['email']
+       subject=request.form['subject']
+       msg=request.form['message']
+       send_email(from_name, from_email, subject, msg)
+       flash('Email sucessfully sent')
+       return redirect(url_for('home'))
+    else:
+       return render_template('contact.html' , error=error)
+    
+def send_email(from_name, from_email, subject, msg):
+    to_addr  = 'someemail@yahoo.com' 
+    to_name = 'Alison Thomas'
+    
+    message = """From: {} <{}> To: {} <{}> 
+              Subject: {} 
+              {} """
+    message_to_send = message.format(from_name, from_email, to_name, to_addr, subject, msg)
 
+    # Credentials (if needed)
+    username = 'email'
+    password = 'password'
+
+    # The actual mail send
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(username, password)
+    server.sendmail(from_email, to_addr, message_to_send)
+    server.quit() 
+    
+    
 ###
 # The functions below should be applicable to all Flask apps.
 ###
